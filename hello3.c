325e612f6d0a9e7afd41c37683e76df3ea4d7712
pradeep7997 <84910809+pradeep7997@users.noreply.github.com>
2023-09-26 16:33:48 -0500

    Update hello3.c

--- hello3.c
+++ hello3.c
@@ -6,9 +6,3 @@ int main()
     fprintf(stderr, "this is an error\n");
     return 1;
 }
-
-// Compile, then run `./a.out 1>stdout.txt 2>stderr.txt`
-// Then run `echo $?`
-// stdout.txt should contain "hello, world"
-// stderr.txt should contain "this is an error"
-// Your `echo $?` command should return 1
